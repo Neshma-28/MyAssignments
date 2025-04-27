c261fc293614d6099bc81555ca34e61da27ee439
Neshma-28 <neshma.tsm@gmail.com>
2025-04-27 11:34:13 +0530

    Add files via upload
    day1

---
+++ day1/chrome.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		
+System.out.println("variable of various datatypes and values");
+//float
+float version=91.0F;
+System.out.println(version);
+//string
+String developer="Google";
+System.out.println(developer);
+//boolean
+Boolean isBeta=false;
+System.out.println(isBeta);
+//int
+int releaseYear=2008;
+System.out.println(releaseYear);
+//char
+char shortcutKey='C';
+System.out.println(shortcutKey);
+
+	}
+
+}
