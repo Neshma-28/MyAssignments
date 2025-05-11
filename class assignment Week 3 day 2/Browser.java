6a4f1bc0a0b58d32ef0baf6f17b8cb3fc00d867a
Neshma-28 <neshma.tsm@gmail.com>
2025-05-11 18:57:07 +0530

    Update Browser.java

--- class assignment Week 3 day 2/Browser.java
+++ class assignment Week 3 day 2/Browser.java
@@ -2,12 +2,15 @@ package week3.day2;
 //Super Class
 public class Browser {
 	//Defined Local variable
-     String BrowserName="CHROME";
-	 float Browserversion=91.0F;
+	String browser1="Chrome";
+	String browser2="Edge";
+	String browser3="Safari";
+	String browserName= browser1+" "+browser2+" "+browser3;
+	float Browserversion=91.0F;
 	//Define Method
 	public void openurl() {
 	
-	System.out.println(BrowserName);
+	System.out.println(browserName);
 			
 	}
 	//Define Method
